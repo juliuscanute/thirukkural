32b1c0f78e16d1ab7b6e2ded31cee751c3667f64
Julius Canute <juliuscanute@gmail.com>
2026-01-20 08:46:25 +1100

    Add navigation bar padding

--- app/src/main/java/com/juliuscanute/tkural/MainActivity.kt
+++ app/src/main/java/com/juliuscanute/tkural/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -85,6 +86,7 @@ class MainActivity : AppCompatActivity() {
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
+                            .navigationBarsPadding()
                             .padding(16.dp),
                         horizontalArrangement = Arrangement.SpaceBetween
                     ) {
